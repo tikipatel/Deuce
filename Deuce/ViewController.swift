3353ae0bf45700c00a8d50198bfdf14f9357a908
Austin Conlon <austinconlon@Austins-MacBook-Pro.local>
2016-11-27 00:57:46 -0800

    Initial Commit

---
+++ Deuce/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Deuce
+//
+//  Created by Austin Conlon on 11/27/16.
+//  Copyright © 2016 Austin Conlon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
